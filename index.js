70bdb2a4136e67cff43625fa213da51b9eb352be
james <uchennaanih16@gmail.com>
2021-11-20 10:55:34 +0100

    update default display

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Transtura Assessment");
 });
 
 app.use("/api/", routes);
--- src/controllers/riders.controller.js
+++ src/controllers/riders.controller.js
@@ -1,11 +1,19 @@
 const Riders = require("../models").Riders;
 exports.getAllRiders = async (req, res, next) => {
   try {
-    console.log("here");
-    Riders.findAll({}).then((riders) => {
-      return res.status(200).json(riders);
+    const riders = await Riders.findAll();
+    return res.status(200).json({
+      error: false,
+      message: "riders retrieved",
+      data: riders,
+      statusCode: 200,
     });
   } catch (error) {
     console.log(error);
+    return res.status(500).jsons({
+      error: true,
+      message: "Internal Server Error",
+      statusCode: 500,
+    });
   }
 };
